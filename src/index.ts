1b54acd28806e2cbe30ab968e6d7a6f4e4c02ea4
peter.gemmell <peter.gemmell@mths.ca>
2023-01-02 21:50:14 +0000

    Wagwan

--- src/index.ts
+++ src/index.ts
@@ -1,139 +1,78 @@
-import { writeFileSync } from "fs"
-
-function generateGaussian(mean: number ,std: number){
-  // https://discourse.psychopy.org/t/javascript-gaussian-function/17724/2
-        var _2PI = Math.PI * 2;
-  var u1 = Math.random();
-  var u2 = Math.random();
-  
-  var z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(_2PI * u2);
-  var z1 = Math.sqrt(-2.0 * Math.log(u1)) * Math.sin(_2PI * u2);
-
-  return z0 * std + mean;
-}
-
-let sum = 0
-let numbers = ''
-
-// Generate 10,000 Gaussian random numbers
-for (var i = 0; i < 100; i++) {
-  const normalNumber = generateGaussian(75, 10)
-  sum = sum + normalNumber
-  numbers = numbers + normalNumber + '\n'
-  console.log(normalNumber)
-}
-
-console.log("\n")
-console.log(sum / i)
-console.log("\n")
-console.log(numbers)
-writeFileSync("NormalNumbers.txt", numbers)
-
-
-// Java
-
-/*
-* This is a program generates marks
-* after reading in 2 text files.
-*
-* @author  Mr Coxall
-* @version 1.0
-* @since   2020-01-01
-*/
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
 /**
-* This is the marks program.
-*/
-final class Marks {
+ * Spit out csv file with grades
+ *
+ * By: Peter
+ * Version: 1.0
+ * Since: 2023-01-1
+ */
 
-    /**
-    * Prevent instantiation
-    * Throw an exception IllegalStateException.
-    * if this ever is called
-    *
-    * @throws IllegalStateException
-    *
-    */
-    private Marks() {
-        throw new IllegalStateException("Cannot be instantiated");
-    }
+import { writeFileSync } from 'fs'
+import { readFileSync } from 'fs'
 
-    /**
-    * The generateMarks() function.
-    *
-    * @param arrayOfStudents the collection of students
-    * @param arrayOfAssignments the collection of assignments
-    * @return the generated marks
-    */
-    public static String[][] generateMarks(final Integer[] arrayOfStudents, 
-                                       final Integer[] arrayOfAssignments) {
+function generateGaussian(mean: number, std: number) {
+  // https://discourse.psychopy.org/t/javascript-gaussian-function/17724/2
+  let _2PI = Math.PI * 2
+  let u1 = Math.random()
+  let u2 = Math.random()
 
-        // this is just a place holder!
-        String[][] markArray = { { "", "Ass #1", "Ass #2" }, 
-                           { "Sue", "76%", "88%" },
-                           { "Bob", "46%", "81%" } };
+  let z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(_2PI * u2)
 
-        return markArray;
-    }
-
-    /**
-    * The starting main() function.
-    *
-    * @param args No args will be used
-    */
-    public static void main(final String[] args) {
-        final ArrayList<String> listOfStudents = new ArrayList<String>();
-        final ArrayList<String> listOfAssingments = new ArrayList<String>();
-        final Path studentFilePath = Paths.get("../", args[0]);
-        final Path assignmentFilePath = Paths.get("../", args[1]);
-        final Charset charset = Charset.forName("UTF-8");
-
-        try (BufferedReader readerStudent = Files.newBufferedReader(
-                                     studentFilePath, charset)) {
-            String lineStudent = null;
-            while ((lineStudent = readerStudent.readLine()) != null) {
-                listOfStudents.add(lineStudent);
-                System.out.println(lineStudent);
-            }
-        } catch (IOException errorCode) {
-            System.err.println(errorCode);
-        }
-
-        try (BufferedReader readerAssignment = Files.newBufferedReader(
-                                     assignmentFilePath, charset)) {
-            String lineAssignment = null;
-            while ((lineAssignment = readerAssignment.readLine()) != null) {
-                listOfAssingments.add(lineAssignment);
-                System.out.println(lineAssignment);
-            }
-        } catch (IOException errorCode) {
-            System.err.println(errorCode);
-        }
-
-        // Normal Distribution numbers
-        Random random = new Random();
-
-        for (int loopCounter = 0; loopCounter < 5; loopCounter++) {
-            int mark = (int)Math.floor(random.nextGaussian()*10+75);
-            System.out.println(mark);
-        }
-
-        // 
-
-        System.out.println("\nDone.");
-    }
+  return z0 * std + mean
 }
 
+function makeArray(d1: number, d2: number) {
+  // https://stackoverflow.com/questions/13808325/creating-a-2d-array-with-specific-length-and-width
+  let arr = []
+  for (let i = 0; i < d2; i++) {
+    arr.push(new Array(d1))
+  }
+  return arr
+}
+
+// Function that grades and creates array
+function grader(assArr: string[], namesArr: string[]) {
+  // Defining array
+  const arr: string[][] = makeArray(namesArr.length + 1, arrAss.length)
+
+  // Adding student names
+  arr[0] = arrNames
+  arr[0].unshift('')
+
+  // Adding assignments and grades
+  for (let y = 1; y <= assArr.length - 1; y++) {
+    arr[y][0] = arrAss[y]
+    for (let x = 1; x <= namesArr.length - 1; x++) {
+      arr[y][x] = Math.round(generateGaussian(75, 10)).toString()
+    }
+  }
+
+  // Return array
+  return arr
+}
+
+// Read files
+const namesFile = readFileSync('./names1.txt', 'utf-8')
+const assFile = readFileSync('./units1.txt', 'utf-8')
+
+// Convert files to arr
+const arrNames = namesFile.toString().replace(/\r\n/g, '\n').split('\n')
+arrNames.pop()
+const arrAss = assFile.toString().replace(/\r\n/g, '\n').split('\n')
+arrAss.pop()
+
+// Call function
+const returned: string[][] = grader(arrAss, arrNames)
+
+// Export string
+let exportString: string = ''
+
+for (let y = 0; y < returned.length; y++) {
+  for (let x = 0; x < returned[0].length; x++) {
+    exportString += returned[y][x]
+    exportString += ','
+  }
+  exportString += '\n'
+}
+
+// Spit out to file
+writeFileSync('table.csv', exportString)
